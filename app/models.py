8575909db3c5b7162ea602d80a9b455a19db1c52
maureenndiema <ndiemam@gmail.com>
2022-05-17 12:15:31 +0300

    create user class in models.py

---
+++ app/models.py
@@ -0,0 +1,31 @@
+from . import db, Loginmanager
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash,check_password_hash
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), nullable=False, unique=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    password_hash = db.Column(db.String(255), nullable=False)  
+    profile_pic_path = db.Column(db.String) 
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+    
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+        
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    
+    def __repr__(self):
+        return f'User {self.username}'
+
+
+
+
