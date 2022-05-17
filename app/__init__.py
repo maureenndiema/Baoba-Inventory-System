a2ab97d4a2ce919cc4d150c47f0f74dfd71bcd58
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-05-17 22:41:59 +0300

    Feat: Initialize flask extensions

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,16 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
-# from flask_login import LoginManager
+from flask_login import LoginManager
 from flask_uploads import IMAGES, UploadSet,configure_uploads
 from flask_mail import Mail
 
+# login manager
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
+mail = Mail()
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 
@@ -16,13 +21,17 @@ def create_app(config_name):
     app.config.from_object(config_options[config_name])
     app.config['SQLALCHEMY_DATABASE_URI']='postgresql+psycopg2://maureen:1234@localhost/baoba'
 
-
+# initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    mail.init_app(app)
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-
+    
+    # configuration uploads
+    
     # from .auth import auth as authentication_blueprint
     # app.register_blueprint(authentication_blueprint, url_prefix='/authenticate')
 
