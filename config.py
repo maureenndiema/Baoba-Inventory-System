c83b1698ec91fbe74a3224b96b61c769e142df06
maureenndiema <ndiemam@gmail.com>
2022-05-17 13:14:15 +0300

    create class tesst configuration

--- config.py
+++ config.py
@@ -13,3 +13,11 @@ class ProdConfig(Config):
     Config: The parent configuration class with General configuration settings
     '''
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")  
+
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+    Args:
+    Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = ''
