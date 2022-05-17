d4e6cb951c8b7c6d31a152761d47e31e910ff8c0
maureenndiema <ndiemam@gmail.com>
2022-05-17 13:26:43 +0300

    blueprint authentication

--- app/__init__.py
+++ app/__init__.py
@@ -22,3 +22,6 @@ def create_app(config_name):
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as authentication_blueprint
+    app.register_blueprint(authentication_blueprint, url_prefix='/authenticate')
