d3a5276e88b5b8f659dcc234f6717e39676da821
maureenndiema <ndiemam@gmail.com>
2022-05-17 13:08:24 +0300

    set up manager run

--- manage.py
+++ manage.py
@@ -14,3 +14,10 @@ def test():
     import unittest
     tests=unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
+
+if __name__ == '__main__':
+    manager.run()
