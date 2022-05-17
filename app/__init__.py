72156e73e4449783b1bd85d4e9520f4f0ef52e62
maureenndiema <ndiemam@gmail.com>
2022-05-17 13:23:03 +0300

    create_app function

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import IMAGES, UploadSet,configure_uploads
 from flask_mail import Mail
+
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    app.config.from_object(config_options[config_name])
+    app.config['SQLALCHEMY_DATABASE_URI']=''
